706dfccf98d8fd1103699aed097d3e38e621f92c
Kgaphola45 <It45ma@protonmail.ch>
2025-08-16 16:53:38 +0200

    `Added initial JavaScript code for theme and hamburger button functionality`

---
+++ script.js
@@ -0,0 +1,9 @@
+const body = document.body;
+
+const btnTheme = document.querySelector(".fa-moon");
+const btnHamburger = document.querySelector(".fa-bars");
+
+const addThemeClass = (bodyClass, btnClass) => {
+  body.classList.add(bodyClass);
+  btnTheme.classList.add(btnClass);
+};
