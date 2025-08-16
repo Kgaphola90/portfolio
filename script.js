66a1269ebb21840c28f035f1169c64378c18fc48
Kgaphola45 <It45ma@protonmail.ch>
2025-08-16 16:56:27 +0200

    `Added event listener for hamburger button and functionality to display scroll-to-top button on scroll.`

--- script.js
+++ script.js
@@ -42,3 +42,17 @@ const displayList = () => {
     navUl.classList.remove("display-nav-list");
   }
 };
+
+btnHamburger.addEventListener("click", displayList);
+
+const scrollUp = () => {
+  const btnScrollTop = document.querySelector(".scroll-top");
+
+  if (body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+    btnScrollTop.style.display = "block";
+  } else {
+    btnScrollTop.style.display = "none";
+  }
+};
+
+document.addEventListener("scroll", scrollUp);
